1a70da5ccdc70789592f5471d06e5951190e5f4f
Lina <londonohidalgo@gmail.com>
2024-09-24 16:46:29 +0200

    post basket

---
+++ routes/baskets.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment');
+
+const Trip = require('../models/trips');
+
+//Récuperer les trips suite au click "Book"
+
+router.post('/', (req, res) => {
+    let dateRec= moment(req.body.date).format('DD-MM-YYYY, h:mm:ss a');
+
+	Trip.findOne({departure: { $regex: new RegExp(req.body.departure, 'i') }, arrival: { $regex: new RegExp(req.body.arrival, 'i')}})
+    .then(data => {
+        if(data.length > 0) {
+            let tripsDate = []
+            
+            for( let obj of data){
+              let dbDate = moment.utc(obj.date).format('DD-MM-YYYY, h:mm:ss a');
+              if( dbDate === dateRec){
+                tripsDate.push({
+                  departure: obj.departure,
+                  arrival: obj.arrival,
+                  heure: moment.utc(obj.date).format('h:mm a'),
+                  price: price
+                });
+              }};
+            
+            if (tripsDate == []){
+              return res.json({ result: false, error: "Trip not found" })
+              }
+              else {
+                return res.json({result: true, trips : tripsDate})
+              }
+          }
+          else {
+            return res.json({ result: false, error: "No tickets in your card" })
+          }
+        })
+      }); 
+      
+
+
+module.exports = router;
