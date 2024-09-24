f4e68cad9b894b1afdb0516cb44b85f3e7818f84
Lina <londonohidalgo@gmail.com>
2024-09-24 11:34:04 +0200

    routes

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require('../models/connection');
+const Trip = require('../models/trips');
+
+// GET Liste de tous les Trips disponibles
+
+router.get('/', (req, res) => {
+	Trip.find().then(data => {
+		res.json({ trips: data });
+	});
+});
+
+// Selectionner un Trip : departure, arrival et date
+router.post('/', (req, res) => {
+	
+	Trip.findOne({ departure: { $regex: new RegExp(req.body.departure, 'i') }, arrival: { $regex: new RegExp(req.body.arrival, 'i') }, date:{ $regex: new RegExp(req.body.date, 'i') } })
+  .then(data => {
+		if (data === null) {
+      res.json({ result: false, error: "City not found" })
+    }
+    else {
+      res.json({ result: true, trip: data })
+    }
+  });
 });
 
 module.exports = router;
