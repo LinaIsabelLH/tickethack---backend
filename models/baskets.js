af6d419ab5cf6bf24d81d872b5311ab55bf8fa49
Lina <londonohidalgo@gmail.com>
2024-09-25 10:41:21 +0200

    avance

--- models/baskets.js
+++ models/baskets.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const basketSchema = mongoose.Schema({
     tripsId: [{type: mongoose.Schema.Types.ObjectId, ref:'trips'}],
     time: Date, 
-    user : String
 	//{type: mongoose.Schema.Types.ObjectId, ref:'users'}
 });
 
--- models/bookings.js
+++ models/bookings.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const bookingSchema = mongoose.Schema({
 	reservation: [{type: mongoose.Schema.Types.ObjectId, ref:'trips'}],
     time: Date,
-    user: 'Lina'
     //{type: mongoose.Schema.Types.ObjectId, ref:'users'},
 	
 });
