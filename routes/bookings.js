bb49851af7e3c1de0c9600b81c32972fa702e5e6
Lina <londonohidalgo@gmail.com>
2024-09-25 10:14:50 +0200

    route bookings

---
+++ routes/bookings.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment');
+
+const Trip = require('../models/trips');
+const Basket = require('../models/baskets');
+const Booking = require('../models/bookings');
+
+
+
+
+
+
+module.exports = router;
